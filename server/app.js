04277df4e57b1ac53611f5555b035e0af07dc9b6
Junwon Jung <junwon3329@gmail.com>
2019-03-09 03:32:27 -0600

    starting my own new project

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
