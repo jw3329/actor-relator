0dd58ec03cf459928e82f85ae8b651e07593529e
Junwon Jung <junwon3329@gmail.com>
2019-03-09 19:26:06 -0600

    actor-relator backend implementation, frontend react implementation completed, gathering basic actor info completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const config = require('./config.json');
+
+const App = () => {
+
+  const [actor, setActor] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = await (
+      await axios.get(`${config.server.url}:${config.server.port}`, {
+        params: {
+          name: actor
+        }
+      })
+    ).data;
+    console.log(data);
   }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="d-flex align-content-center flex-wrap mt-5">
+          <div className="card text-center mx-auto">
+            <div className="card-body p-5">
+              <h5 className="card-title">Actor Relator</h5>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group m-3">
+                  <input type="text" className="form-control" onChange={e => setActor(e.target.value)} placeholder="Enter actor name" />
+                </div>
+                <button type="submit" className="btn btn-primary m-2">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,11 +1,74 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    const url = "https://en.wikipedia.org/w/api.php";
+    const searchName = req.query.name;
+    const params = {
+        action: 'opensearch',
+        list: "search",
+        search: searchName,
+        format: "json",
+        limit: 1,
+        namespace: 0
+    }
+    try {
+        const data = (await axios.get(url, {
+            params
+        })).data;
+        if (data[1][0] === searchName) {
+            res.send(await gatherActorInfo(data[3][0]));
+        } else {
+            res.send("Your search page '" + searchName + "' does not exists on English Wikipedia");
+        }
+    } catch (error) {
+        res.send(error);
+    }
 });
 
-const port = 8080;
+const gatherActorInfo = async url => {
+    try {
+        const html = (await axios.get(url)).data;
+        const $ = cheerio.load(html);
+        const infobox = $('.infobox.biography.vcard');
+        const tr = $(infobox).contents().children('tr');
+        const name = $(tr).find('.fn').text();
+        const bday = $(tr).find('.bday').text();
+        const occupation = $(tr).find('.role').text();
+        const allegiance = $(tr).find('.flagicon').first().parent().text().trim();
+        console.log(allegiance);
+        const filmList = $('.div-col.columns.column-width');
+        const movie = [];
+        const li = $(filmList).find('ul').children('li');
+        const wikiUrl = 'https://en.wikipedia.org';
+        li.each((_, elem) => {
+            // console.log(elem);
+            const a = $(elem).find('a');
+            const url = $(a).attr('href');
+            const title = $(a).text();
+            movie.push({
+                movieUrl: wikiUrl + url,
+                movieTitle: title
+            });
+        });
+        const actor = {
+            name,
+            bday,
+            occupation,
+            allegiance,
+            movie
+        }
+        return actor
+    } catch (error) {
+        return error;
+    }
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
